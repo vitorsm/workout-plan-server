ad7a10d264a057a1185f259dc5a74abd23792e0a
Vitor Moreira <vitor.moreira@syngenta.com>
2021-11-15 17:31:04 -0300

    adding slash to path

--- workout_plan_server/utils/file_utils.py
+++ workout_plan_server/utils/file_utils.py
@@ -9,6 +9,6 @@ def get_project_dir() -> str:
     if path_github_action_with_tests in dir_path:
         dir_path = path_github_action + dir_path.split(path_github_action_with_tests)[1]
 
-    result = dir_path.split("workout-plan-server")[0] + "workout-plan-server"
+    result = dir_path.split("workout-plan-server")[0] + "/workout-plan-server"
 
     return result
