57a093bd6f7cf8a1d15305f9a90ae9d5bc73115d
Vitor Moreira <vitor.moreira@syngenta.com>
2021-11-15 17:39:59 -0300

    changing project dir for test runner

--- workout_plan_server/utils/file_utils.py
+++ workout_plan_server/utils/file_utils.py
@@ -2,17 +2,9 @@ import os
 
 
 def get_project_dir() -> str:
+    # /home/runner/work/workout-plan-server/workout-plan-server/tests/integration_tests/base_test.py
+
     dir_path = os.getcwd()
-    path_github_action = "/github/workspace"
-    path_github_action_with_tests = path_github_action + "/tests"
-
-    is_github_action = path_github_action_with_tests in dir_path
-    if is_github_action:
-        dir_path = path_github_action + dir_path.split(path_github_action_with_tests)[1]
-
-    result = dir_path.split("workout-plan-server")[0]
-
-    if not is_github_action:
-        result = result + "workout-plan-server"
+    result = dir_path.split("workout-plan-server")[0] + "workout-plan-server"
 
     return result
