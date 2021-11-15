909f7184aacb17ef39d2462427ea77b5338c30e2
Vitor Moreira <vitor.moreira@syngenta.com>
2021-11-15 17:29:48 -0300

    fixing get project dir

--- workout_plan_server/utils/file_utils.py
+++ workout_plan_server/utils/file_utils.py
@@ -3,4 +3,12 @@ import os
 
 def get_project_dir() -> str:
     dir_path = os.getcwd()
-    return dir_path.split("workout-plan-server")[0] + "/workout-plan-server"
+    path_github_action = "/github/workspace"
+    path_github_action_with_tests = path_github_action + "/tests"
+
+    if path_github_action_with_tests in dir_path:
+        dir_path = path_github_action + dir_path.split(path_github_action_with_tests)[1]
+
+    result = dir_path.split("workout-plan-server")[0] + "workout-plan-server"
+
+    return result
